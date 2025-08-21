0c2adf3d0fd8d85348b07618bd0a8b15b11297db
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2025-08-21 18:36:20 -0300

    feat: agregue entidad propietario y controlador del propietario

---
+++ Models/Propietario.cs
@@ -0,0 +1,27 @@
+
+using System.ComponentModel.DataAnnotations;
+
+public class Persona
+{
+    public int id_propietario { get; set; }
+    public int dni_propietario { get; set; }
+    public string contrasena_propietario { get; set; }
+    public string nombre_propietario { get; set; }
+    public string apellido_propietario { get; set; }
+    public string email_propietario { get; set; }
+    public string telefono_propietario { get; set; }
+
+    public Persona(int id_propietario, int dni_propietario, string contrasena_propietario, string nombre_propietario, string apellido_propietario, string email_propietario, string telefono_propietario)
+    {
+        this.id_propietario = id_propietario;
+        this.dni_propietario = dni_propietario;
+        this.contrasena_propietario = contrasena_propietario;
+        this.nombre_propietario = nombre_propietario;
+        this.apellido_propietario = apellido_propietario;
+        this.email_propietario = email_propietario;
+        this.telefono_propietario = telefono_propietario;
+    }
+
+
+
+}
