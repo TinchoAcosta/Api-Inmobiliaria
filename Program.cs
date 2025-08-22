b19614c1cc57df391ac072689ad36be81ac23b03
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2025-08-21 22:31:57 -0300

    feat: agregue carpet propietario para las vistas

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Registrar el servicio RepositorioPropietario
+builder.Services.AddScoped<RepositorioPropietario, RepositorioPropietario>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
