850313ff89d12a851306ca5b4786b4a34a43e256
TinchoAcosta <soymartinacosta@hotmail.es>
2025-08-28 17:34:27 -0300

    feat: database actualizada

--- database/inmobiliaria.sql
+++ database/inmobiliaria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-08-2025 a las 00:42:07
+-- Tiempo de generación: 28-08-2025 a las 22:32:54
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -37,7 +37,9 @@ CREATE TABLE `inmueble` (
   `lat_inmueble` decimal(10,8) NOT NULL,
   `long_inmueble` decimal(11,8) NOT NULL,
   `PropietarioId` int(11) NOT NULL,
-  `portada_inmueble` varchar(100) DEFAULT NULL
+  `portada_inmueble` varchar(100) DEFAULT NULL,
+  `tipo_inmueble` int(11) NOT NULL,
+  `uso_inmueble` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -64,7 +66,8 @@ INSERT INTO `inquilino` (`id_inquilino`, `nombre_inquilino`, `apellido_inquilino
 (1, 'Lucía', 'Fernández', 40123456, '2664001122', 'lucia.fernandez@example.com', 1),
 (2, 'Gonzalo', 'Juarez', 40234567, '2664003344', 'carlos.ramirez@example.com', 1),
 (3, 'inquilino', 'inquilinoapellido', 1112223, '65465464656', 'inquilino@inquilino.inquilino', 1),
-(4, 'asa', 'sas', 123123421, '123213', 'asdasdadsadsa@asas.asa', 0);
+(4, 'asa', 'sas', 123123421, '123213', 'asdasdadsadsa@asas.asa', 0),
+(5, 'borrado_inquilino', 'sas', 134213, '123213', 'asdasdadsadsa@asas.asa', 0);
 
 -- --------------------------------------------------------
 
@@ -89,7 +92,30 @@ CREATE TABLE `propietario` (
 
 INSERT INTO `propietario` (`id_propietario`, `dni_propietario`, `contrasena_propietario`, `nombre_propietario`, `apellido_propietario`, `email_propietario`, `telefono_propietario`, `borrado_propietario`) VALUES
 (1, 30123456, 'clave123', 'María', 'Gómez', 'maria.gomez@example.com', '2664123456', 1),
-(2, 30234567, 'segura456', 'Juan', 'Pérez', 'juan.perez@example.com', '2664987654', 1);
+(2, 30234567, 'segura456', 'Juan', 'Pérez', 'juan.perez@example.com', '2664987654', 1),
+(3, 12312, 'adsasd', 'J', 'Perez', 'propietario@p.prop', '123213', 1),
+(4, 213412, 'fdsfds', 'prop2', 'asdaddsa', 'asdads@asd.s', '123213', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_inmueble`
+--
+
+CREATE TABLE `tipo_inmueble` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `tipo_inmueble`
+--
+
+INSERT INTO `tipo_inmueble` (`id`, `descripcion`) VALUES
+(1, 'Deposito'),
+(2, 'Local'),
+(3, 'Casa'),
+(4, 'Departamento');
 
 --
 -- Índices para tablas volcadas
@@ -100,7 +126,8 @@ INSERT INTO `propietario` (`id_propietario`, `dni_propietario`, `contrasena_prop
 --
 ALTER TABLE `inmueble`
   ADD PRIMARY KEY (`id_inmueble`),
-  ADD KEY `PropietarioId` (`PropietarioId`);
+  ADD KEY `PropietarioId` (`PropietarioId`),
+  ADD KEY `tipo_inmueble` (`tipo_inmueble`);
 
 --
 -- Indices de la tabla `inquilino`
@@ -113,6 +140,12 @@ ALTER TABLE `inquilino`
 --
 ALTER TABLE `propietario`
   ADD PRIMARY KEY (`id_propietario`);
+
+--
+-- Indices de la tabla `tipo_inmueble`
+--
+ALTER TABLE `tipo_inmueble`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -128,13 +161,19 @@ ALTER TABLE `inmueble`
 -- AUTO_INCREMENT de la tabla `inquilino`
 --
 ALTER TABLE `inquilino`
-  MODIFY `id_inquilino` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_inquilino` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `propietario`
 --
 ALTER TABLE `propietario`
-  MODIFY `id_propietario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_propietario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_inmueble`
+--
+ALTER TABLE `tipo_inmueble`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restricciones para tablas volcadas
@@ -144,7 +183,8 @@ ALTER TABLE `propietario`
 -- Filtros para la tabla `inmueble`
 --
 ALTER TABLE `inmueble`
-  ADD CONSTRAINT `inmueble_ibfk_1` FOREIGN KEY (`PropietarioId`) REFERENCES `propietario` (`id_propietario`);
+  ADD CONSTRAINT `inmueble_ibfk_1` FOREIGN KEY (`PropietarioId`) REFERENCES `propietario` (`id_propietario`),
+  ADD CONSTRAINT `inmueble_ibfk_2` FOREIGN KEY (`tipo_inmueble`) REFERENCES `tipo_inmueble` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
